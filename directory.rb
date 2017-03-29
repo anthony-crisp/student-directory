b687ecd87a9f750d313116f3ef5b3a9df16abe2d
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-29 17:43:48 +0100

    Added only print if name is shorter than 12 chars

--- directory.rb
+++ directory.rb
@@ -18,7 +18,7 @@ end
 
 def print(students)
     students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == "l"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].size < 12
   end
 end
 
