0418dc52540c20d5edca6f9b81c8be1590d9791f
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-03-29 17:40:35 +0100

    Added only print names beginning with a given letter

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
     students = []
     name = gets.chomp
     while !name.empty? do
-      students << {name: name,cohort: :november }
+      students << {name: name,cohort: :November }
       puts "Now we have #{students.count} students"
       name = gets.chomp
   end
@@ -18,7 +18,7 @@ end
 
 def print(students)
     students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].downcase == "l"
   end
 end
 
