c18186e25e2b6c11509a5795034b80e913caffc2
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-04-01 19:04:51 +0100

    Center adjusted text and if statment if no students added.

--- directory.rb
+++ directory.rb
@@ -1,14 +1,14 @@
 def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
+    puts "Please enter the names of the students".center(100)
+    puts "To finish, just hit return twice".center(100)
     students = []
     name = gets.chomp
     while !name.empty? do
       students << {name: name,cohort: :November }
       if students.count == 1
-        puts  "Now we have #{students.count} student."
+        puts  "Now we have #{students.count} student.".center(100)
       else
-        puts  "Now we have #{students.count} students."
+        puts  "Now we have #{students.count} students.".center(101)
       end
       name = gets.chomp
   end
@@ -16,23 +16,25 @@ def input_students
 end
 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(100)
+    puts "-------------".center(100)
 end
 
 def print(students)
     i = 0
     until i == students.count
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(100)
     i += 1
   end
 end
 
 def print_footer(names)
   if names.count == 1
-    puts "Overall, we have #{names.count} great student."
+    puts "Overall, we have #{names.count} great student.".center(100)
+  elsif names.count >= 2
+    puts "Overall, we have #{names.count} great students.".center(100)
   else
-    puts "Overall, we have #{names.count} great students."
+    puts "No students enroled at this time.".center(100)
   end
 end
 
