dc4e48a85ff6a1df5369e767f41e785fdb231e90
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-04-09 08:57:52 +0100

    Interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,25 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "Type '1' to input students"
+    puts "Type '2' view students"
+    puts "Type '9' to exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean. Try again."
+    end
+  end
+end
+
 def input_students
     puts "Please enter the names of the students".center(100)
     puts "To finish, just hit return twice".center(100)
@@ -8,7 +30,7 @@ def input_students
       if students.count == 1
         puts  "Now we have #{students.count} student.".center(100)
       else
-        puts  "Now we have #{students.count} students.".center(101)
+        puts  "Now we have #{students.count} students.".center(100)
       end
       name = gets.chomp
   end
@@ -32,13 +54,10 @@ def print_footer(names)
   if names.count == 1
     puts "Overall, we have #{names.count} great student.".center(100)
   elsif names.count >= 2
-    puts "Overall, we have #{names.count} great students.".center(100)
+    puts "Overall, we have #{names.count} great students.".center(101)
   else
     puts "No students enroled at this time.".center(100)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
